fde9cbcf91a334baea63298a4eccbfe0f593dd33
sponge <sponge@d8d.org>
2017-08-03 21:33:53 -0500

    make checkboxes not use the checkbox ui, cross out the titles workflowy style

--- src/markdownviewer.js
+++ src/markdownviewer.js
@@ -40,17 +40,30 @@ class Markdown extends Component {
       remarkReactComponents: {
         li: (props) => {
           // this is awful but it transforms list items that are task lists into checkbox + label
+          let extraClass = '';
           if (props.className && props.className.indexOf('task-list-item') >= 0) {
             const checkbox = props.children.shift(); // the checkbox is the first child
-            const labelChildren = props.children.splice(0); // everything else is the list item
+            const labelChildren = props.children.splice(1); // skip a blank node, everything else is the list item
+            
+            extraClass += checkbox.props.checked !== undefined ? ' complete' : '';
 
             // make a new checkbox and stuff everything else into a label
-            props.children.push(<input type="checkbox" id={'check-'+props.id} key={'check-'+props.id} checked={checkbox.props.checked !== undefined} onChange={(ev) => onChecked(ev.target.parentElement.id)} />);
-            props.children.push(<label key={'label-'+props.id} htmlFor={'check-'+props.id}>{labelChildren}</label>);
+            props.children.push(<input type="checkbox"
+              id={'check-'+props.id}
+              key={'check-'+props.id}
+              checked={checkbox.props.checked !== undefined}
+              onChange={(ev) => onChecked(ev.target.parentElement.id)}
+              style={{display: 'none'}}
+            />);
+
+            props.children.push(<label
+              key={'label-'+props.id}
+              htmlFor={'check-'+props.id}
+            >{labelChildren}</label>);
           }
 
           // just return the same dang thing we'd return before
-          return <li {...props}>{props.children}</li>;
+          return <li {...props} className={props.className + extraClass}>{props.children}</li>;
         },
 
         a: WikiLink
