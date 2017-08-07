068ef4f2efde3723030a3cb37bc02c073246e771
sponge <sponge@d8d.org>
2017-08-07 18:25:26 -0500

    more markdown tutorial

--- src/welcomemessage.js
+++ src/welcomemessage.js
@@ -4,9 +4,9 @@ export default `
 You're seeing this page because you don't have an index.md in your dNoted folder in your Dropbox. Create a new file
 named /index.md and replace me with your own stuff!
 
-Easy. Fast. Beautiful. Three words that this isn't. dNoted is apersonal Wiki that loads and saves directly from your
-Dropbox. Because everything is just a Markdown file, you can editeverything either in dNoted, or any other text editor
-of your choice.
+Easy. Fast. Beautiful. Three words that don't describe this at all. dNoted is a personal Wiki that loads and saves
+directly from your Dropbox. Because everything is just a Markdown file, you can editeverything either in dNoted, or any
+other text editor of your choice.
 
 ## Links
 
@@ -26,8 +26,64 @@ this one because this intro isn't on your Dropbox!)
 - [x] a completed task
 \`\`\`
 
-and they'll render like this:
+and they'll render like this (try clicking them too!)
 
 - [ ] an incomplete task
 - [x] a completed task
+
+## The rest of that Markdown stuff
+
+### Basics
+
+\`\`\`
+*This text will be italic*
+_This will also be italic_
+
+**This text will be bold**
+__This will also be bold__
+
+_You **can** combine them_
+
+~~This text will be striked through~~
+\`\`\`
+
+### Headers
+\`\`\`
+# This is an <h1> tag
+## This is an <h2> tag
+###### This is an <h6> tag
+\`\`\`
+
+### Images
+\`![Image Alt Text](image url)\`
+
+### Lists
+
+#### Unordered
+
+\`\`\`
+- Item 1
+- Item 2
+  - Item 2a
+  - Item 2b
+\`\`\`
+
+#### Ordered
+
+\`\`\`
+1. Item 1
+1. Item 2
+1. Item 3
+   1. Item 3a
+   1. Item 3b
+\`\`\`
+
+### Tables
+
+\`\`\`
+First Header | Second Header
+----------------------------| ----------------------------
+Content from cell 1         | Content from cell 2
+Content in the first column | Content in the second column
+\`\`\`
 `
